80035d10a4c848d7102b56837d5c9768005780aa
Jeferson <Jefersonsinj@gmail.com>
2023-11-28 00:10:12 -0500

    Se agrega la funcionalidad del boton buscarrr

--- cliente.py
+++ cliente.py
@@ -14,6 +14,7 @@ class Cliente:
                 ):
         self.nombreCompleto = nombreCompleto
         self.usuario = usuario
+        #
         self.password = password
         self.documento = documento
         self.correo = correo
